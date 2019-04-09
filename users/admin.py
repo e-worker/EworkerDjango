1823813b013405f96f02f193bb6aee6f83ca4e30
cjaca <ciubajacek@gmail.com>
2019-04-09 13:14:44 +0200

    Register branch, user model

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from django.contrib.auth import get_user_model
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from .forms import CustomUserCreationForm, CustomUserChangeForm
+from .models import CustomUser
+
+class CustomUserAdmin(UserAdmin):
+    add_form = CustomUserCreationForm
+    form = CustomUserChangeForm
+    model = CustomUser
+    list_display = ['email', 'username','isEmployer', 'doneProfileEdit']
+    list_display_links = ['email', 'username',]
+
+admin.site.register(CustomUser, CustomUserAdmin)
--- users/models.py
+++ users/models.py
@@ -1,1 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+class CustomUser(AbstractUser):
+
+    isEmployer = models.BooleanField(default = False)
+    doneProfileEdit = models.BooleanField(default = False, blank = True)
+    def __str__(self):
+        return self.username
