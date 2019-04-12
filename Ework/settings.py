6fbf9b4a857ab170f592be8e530e70113d24d4d1
avenal <avenalszarosiwy@gmail.com>
2019-04-12 13:27:20 +0200

    modified project structure (added staticfiles dir and mediafiles dir), also changed db to postgres

--- Ework/settings.py
+++ Ework/settings.py
@@ -79,8 +79,11 @@ WSGI_APPLICATION = 'Ework.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'eworkdb',
+        'USER': 'postgres',
+        'PASSWORD': '123456',
+        'HOST': 'localhost'
     }
 }
 
@@ -117,9 +120,16 @@ USE_L10N = True
 
 USE_TZ = True
 
+#staticfiles
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/2.1/howto/static-files/
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'Ework/static')
+]
 AUTH_USER_MODEL = 'users.CustomUser'
+
+#media settings
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
