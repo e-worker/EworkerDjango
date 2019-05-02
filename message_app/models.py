f6ace7249a30a1d36f2deca9ea2f37a883097e15
cjaca <ciubajacek@gmail.com>
2019-05-02 13:22:33 +0200

    Fixed sending messages

--- message_app/models.py
+++ message_app/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 from datetime import datetime
-from employer.models import Company
-from student.models import Student
+from users.models import CustomUser
 # Create your models here.
 
 class Message(models.Model):
@@ -9,7 +8,7 @@ class Message(models.Model):
     header = models.CharField(max_length=50)
     content = models.CharField(max_length=512)
     creation_date = models.DateTimeField(default=datetime.now)
-    student = models.ForeignKey('student.Student', models.DO_NOTHING)
-    company = models.ForeignKey(Company, models.DO_NOTHING)
+    msg_from = models.ForeignKey(CustomUser, related_name='sender', on_delete=models.DO_NOTHING)
+    msg_to = models.ForeignKey(CustomUser, related_name='receiver', on_delete=models.DO_NOTHING)
     def __str__(self):
         return str(self.header)
--- message_app/views.py
+++ message_app/views.py
@@ -4,6 +4,7 @@ from django.contrib.auth.decorators import login_required
 from message_app.models import Message
 from employer.models import Company
 from student.models import Student
+from users.models import CustomUser
 
 # Create your views here.
 
@@ -12,35 +13,58 @@ def new_message(request):
     if request.method == 'POST':
         header = request.POST['header']
         content = request.POST['content']
-        if request.user.isEmployer:
-            student = request.POST['email-adresat']
-            company = request.user.email
+        if len(header) < 3:
+            messages.error(request, 'Temat wiadomości jest za kró†ki')
         else:
-            student = request.user.email
-            company = request.POST['email-adresat']
-        if not Company.objects.filter(email=company).exists() or not Student.objects.filter(email=student).exists():
-            messages.error(request, 'Podany adresat nie istnieje w bazie, sprawdz email')
-        else:
-            company_id = Company.objects.get(email=company).id
-            student_id = Student.objects.get(email=student).id
-            message = Message(header=header, content=content, company_id=company_id,student_id=student_id)
-            message.save()
-            return redirect('messages')
+            if len(content) < 5:
+                messages.error(request, 'Wiadomość jest zbyt krótka')
+            else:
+                if request.user.isEmployer:
+                    student_email = request.POST['email-adresat']
+                    company = request.user
+                    if Student.objects.get(email=student_email):
+                        student = CustomUser.objects.get(email=student_email)
+                        message = Message(header=header, content=content, msg_from=company, msg_to=student)
+                        message.save()
+                        return redirect('messages')
+                    else:
+                        messages.error(request, 'Podany student nie istnieje w bazie, sprawdz email')
+                else:
+                    student = request.user
+                    company_email = request.POST['email-adresat']
+                    if Company.objects.get(email=company_email):
+                        company = CustomUser.objects.get(email=company_email)
+                        message = Message(header=header, content=content, msg_from=student, msg_to=company)
+                        message.save()
+                        return redirect('messages')
+                    else:
+                        messages.error(request, 'Podany pracodawca nie istnieje w bazie, sprawdz email')
     return render(request, 'users/new_message.html')
 
 
 def dashboard(request):
     
-    if request.user.isEmployer:
-        id = Company.objects.get(user_id=request.user.id)
-        query_result = Message.objects.filter(company_id=id)
-        students = query_result.values_list('student_id', flat = True)
-        senders = Student.objects.filter(id__in=students)
-        mylist = zip(senders, query_result)
-        context = {
-           'messages_list': mylist,
-        }
-        return render(request, 'users/messages.html', context)
+    # if request.user.isEmployer:
+    #     id = Company.objects.get(user_id=request.user.id)
+    #     query_result = Message.objects.filter(company_id=id)
+    #     students = query_result.values_list('student_id', flat = True)
+    #     senders = Student.objects.filter(id__in=students)
+    #     mylist = zip(senders, query_result)
+    #     context = {
+    #        'messages_list': mylist,
+    #     }
+    #     return render(request, 'users/messages.html', context)
+    # else:
+    #     id = Student.objects.get(user_id=request.user.id)
+    #     query_result = Message.objects.filter(student_id=id)
+    #     companies = query_result.values_list('company_id', flat = True)
+    #     senders = Company.objects.filter(id__in=companies)
+    #     mylist = zip(senders, query_result)
+    #     context = {
+    #         'messages_list': mylist,
+    #     }
+    #     return render(request, 'users/messages.html', context)
+
 
     return render(request, 'users/messages.html')
 
