796971dd770f5dc9c6306bca70a6f826cebc8f77
avenal <avenalszarosiwy@gmail.com>
2019-05-04 14:49:07 +0200

    matching functions (funkcje zapałkowe)

--- job_offers/models.py
+++ job_offers/models.py
@@ -34,6 +34,44 @@ class JobOffer(models.Model):
     def __str__(self):
         return str(self.title)
 
+    def get_info(self):
+        degree_course_info = OfferDegreeCourse.objects.filter(job_offer=self).values("degree_course__name")
+        skills_info = JobInfo.objects.filter(job_offer=self).values('job_skill__skill__name').distinct()
+        skills = []
+        degree_course = []
+        for skill in skills_info:
+            skills.append(skill["job_skill__skill__name"])
+        for degree in degree_course_info:
+            degree_course.append(degree["degree_course__name"])
+        
+        salary_from = self.sallary_from
+        
+        context = {
+            'degree_course': degree_course,
+            'skills': skills,
+            'salary_from': salary_from,
+        }
+        return context
+
+    def match(self, student: 'student.Student'):
+        student_matching_info = student.get_matching_info()
+        job_offer_matching_info = self.get_info()
+        job_req_skills = len(job_offer_matching_info["skills"]) + 2 # skills + (salary & degree)
+        student_matched_skills = 0
+        for skill in student_matching_info["skills"]:
+            if skill in job_offer_matching_info["skills"]:
+                student_matched_skills+=1
+        if job_offer_matching_info["salary_from"] >= student_matching_info["salary_from"]:
+            student_matched_skills+=1
+        for degree in student_matching_info["degree_course"]:
+            if degree in job_offer_matching_info["degree_course"]:
+                student_matched_skills+=1
+                 
+                
+        percentage = student_matched_skills/job_req_skills
+        match = OfferMatchStudent(student=student, percentage=percentage)
+        return match
+
 class JobOfferLanguage(models.Model):
     id = models.AutoField(primary_key = True)
     language = models.ForeignKey('Language', models.DO_NOTHING)
@@ -49,6 +87,10 @@ class JobOfferSkill(models.Model):
     def __str__(self):
         return str(self.skill)
 
+class OfferMatchStudent(models.Model):
+    id = models.AutoField(primary_key = True)
+    student = models.ForeignKey('student.Student', models.DO_NOTHING)
+    percentage = models.FloatField()
 
 class LanguageLvl(models.Model):
     id = models.AutoField(primary_key = True)
--- student/models.py
+++ student/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from datetime import datetime
-from job_offers.models import DegreeCourse, Language, Skill
+from job_offers.models import DegreeCourse, Language, Skill, JobOffer
 from users.models import CustomUser
 # Create your models here.
 class Student(models.Model):
@@ -22,6 +22,58 @@ class Student(models.Model):
     def __str__(self):
         return str(self.name)+" "+str(self.surname)
 
+    def get_matching_info(self):
+        """matching categories
+            degree course
+            experience
+            internship
+            organizations
+            courses
+            ca$h
+        """
+        degree_course_info = StudentDegreeCourse.objects.filter(student=self).values("degree_course__name")
+        skills_info = StudentInfo.objects.filter(student=self).values('skill__skill__name').distinct()
+        skills = []
+        degree_course = []
+        for skill in skills_info:
+            skills.append(skill["skill__skill__name"])
+        for degree in degree_course_info:
+            degree_course.append(degree["degree_course__name"])
+        salary_from = self.salary_from   
+
+        #bierz po jednym, pozniej zrob slownik z ofertami i  do kazdej oferty obliczaj dopasowanie
+
+        context = {
+            'degree_course': degree_course,
+            'skills': skills,
+            'salary_from': salary_from,
+        }
+        return context
+
+    def match(self, job_offer: JobOffer):
+        student_matching_info = self.get_matching_info()
+        job_offer_matching_info = job_offer.get_info()
+        job_req_skills = len(job_offer_matching_info["skills"]) + 2 # skills + (salary & degree)
+        student_matched_skills = 0
+        for skill in student_matching_info["skills"]:
+            print(skill)
+            if skill in job_offer_matching_info["skills"]:
+                print("added")
+                student_matched_skills+=1
+        if job_offer_matching_info["salary_from"] >= student_matching_info["salary_from"]:
+            print("salary matching")
+            student_matched_skills+=1
+        for degree in student_matching_info["degree_course"]:
+            if degree in job_offer_matching_info["degree_course"]:
+                student_matched_skills+=1
+                print(degree)
+            
+                
+        percentage = student_matched_skills/job_req_skills
+        match = StudentMatchOffer(offer=job_offer, percentage=percentage)
+        return match
+
+
 class StudentDegreeCourse(models.Model):
     id = models.AutoField(primary_key = True)
     student = models.ForeignKey(Student, models.DO_NOTHING)
@@ -41,6 +93,10 @@ class StudentInfo(models.Model):
     def __str__(self):
         return str(self.student)
 
+class StudentMatchOffer(models.Model):
+    id = models.AutoField(primary_key = True)
+    offer = models.ForeignKey(JobOffer, models.DO_NOTHING)
+    percentage = models.FloatField()
 
 class StudentLanguage(models.Model):
     id = models.AutoField(primary_key = True)
