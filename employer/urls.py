cab6e8e35013d66669aa9202377961661c6e4af4
cjaca <ciubajacek@gmail.com>
2019-05-22 12:33:00 +0200

    Fixed ulrs on find student

--- employer/urls.py
+++ employer/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from . import views
+from student import views as studentviews
 urlpatterns = [
     path('find_student', views.find_students, name='find_student'),
     path('offer/<int:id>', views.offer, name='offer'),
     path('add_offer', views.add_offer, name="add_offer"),
     path('my_offers', views.employer_offers, name="employer_offers"),
     path('delete_offer/<int:id>', views.delete_offer, name="delete_offer"),
+    path('student_profile/<int:id>', studentviews.student_profile, name="student_profile"),
 ]
